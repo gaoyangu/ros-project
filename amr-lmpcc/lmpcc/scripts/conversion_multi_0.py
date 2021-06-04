9ae8b136948ba27dbb597042f472eba0faa65589
gaoyangu <1600314850@qq.com>
2021-06-04 20:45:59 +0800

    add_env: oneway_with_two_jackal

--- amr-lmpcc/lmpcc/scripts/conversion_multi_0.py
+++ amr-lmpcc/lmpcc/scripts/conversion_multi_0.py
@@ -7,53 +7,41 @@ import message_filters
 from nav_msgs.msg import Path
 from lmpcc_msgs.msg import lmpcc_obstacle_array, lmpcc_obstacle
 
-def callback(path_01, path_02):
+def callback(path_01):
     # rospy.loginfo("new Message")
-
+    path_list = [path_01]
     obstacle_array = lmpcc_obstacle_array()
     obstacle_array.header = path_01.header
 
-    obstacle = lmpcc_obstacle()
-    obstacle_01 = lmpcc_obstacle()
-    obstacle_01.trajectory.poses = path_01.poses
-    for pose in path_01.poses:
-        # rospy.loginfo("%.2f" % (pose.pose.position.x))
-        obstacle_01.major_semiaxis.append(0.2)
-        obstacle_01.minor_semiaxis.append(0.2)
-    obstacle_array.lmpcc_obstacles.append(obstacle_01)
+    for path in path_list:
+        obstacle = lmpcc_obstacle()
+        obstacle.trajectory.poses = path.poses
+        # rospy.loginfo("pose: %.2f " % obstacle.trajectory.poses[0].pose.position.x)
+        for pose in path.poses:
+            obstacle.major_semiaxis.append(0.2)
+            obstacle.minor_semiaxis.append(0.2)
+        obstacle_array.lmpcc_obstacles.append(obstacle)
 
-    obstacle_02 = lmpcc_obstacle()
-    obstacle_02.trajectory.poses = path_02.poses
-    for pose in path_02.poses:
-        # rospy.loginfo("%.2f" % (pose.pose.position.x))
-        obstacle_02.major_semiaxis.append(0.2)
-        obstacle_02.minor_semiaxis.append(0.2)
-    obstacle_array.lmpcc_obstacles.append(obstacle_02)
-
-    # rospy.loginfo("size: %.2f " % len(obstacle_array.lmpcc_obstacles) )
     if len(obstacle_array.lmpcc_obstacles) < 4:
         for obst_it in range(len(obstacle_array.lmpcc_obstacles), 4):
+            obstacle = lmpcc_obstacle()
             obstacle.pose.position.x = -100
             obstacle.pose.position.y = 0
             obstacle.pose.position.z = 0
             obstacle_array.lmpcc_obstacles.append(obstacle)
-    # rospy.loginfo("size: %.2f " % len(obstacle_array.lmpcc_obstacles) )
-
+    # rospy.loginfo("size: %.2f " % len(obstacle_array.lmpcc_obstacles) )        
     pub.publish(obstacle_array)
-    
 
 # Initialize node
 rospy.init_node("conversion_multi_0")
 
 # Create publisher and subscriber
-inputTopic_01 = rospy.resolve_name("/jackal0/predicted_trajectory")
-inputTopic_02 = rospy.resolve_name("/jackal1/predicted_trajectory")
-outputTopic = rospy.resolve_name("/jackal2/ellipse_objects_feed")
+inputTopic_01 = rospy.resolve_name("/jackal1/predicted_trajectory")
+outputTopic = rospy.resolve_name("/jackal0/ellipse_objects_feed")
 
 sub_01 = message_filters.Subscriber(inputTopic_01, Path)
-sub_02 = message_filters.Subscriber(inputTopic_02, Path)
 
-ts = message_filters.TimeSynchronizer([sub_01, sub_02], 10)
+ts = message_filters.TimeSynchronizer([sub_01], 10)
 ts.registerCallback(callback)
 
 pub = rospy.Publisher(outputTopic, lmpcc_obstacle_array, queue_size=5)
